6b34a7462c7d100dc6d50a65e587376bc2ea484c
kabanda.patrick2580 <sendsome2019@gmail.com>
2025-01-17 20:32:58 +0200

    emptied the directory called root

--- alx-project-0x09/pages/api/generate-image.ts
+++ alx-project-0x09/pages/api/generate-image.ts
@@ -1,4 +1,4 @@
-import { HEIGHT, WIDTH } from "@/root/constants";
+import { HEIGHT, WIDTH } from "@/constants";
 import { RequestProps } from "@/interfaces";
 import { NextApiRequest, NextApiResponse } from "next"
 
